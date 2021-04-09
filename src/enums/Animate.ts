23fc5f6b57045744f6f37aa6ad27c638c9f65a73
pwk-czerwinski <pwkczerwinski.work@gmail.com>
2021-04-10 01:41:00 +0200

    Created enum which defines possible animates

---
+++ src/enums/Animate.ts
@@ -0,0 +1,10 @@
+/**
+ * Possible values for data animate.
+ */
+enum Animate {
+    FadeInLeft = 'fadeInLeft',
+    FadeInUp = 'fadeInUp',
+    FadeInRight = 'fadeInRight'
+}
+
+export default Animate;
