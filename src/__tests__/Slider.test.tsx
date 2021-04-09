66914267106e69ac8e532589a81532485f94e63e
pwk-czerwinski <pwkczerwinski.work@gmail.com>
2021-04-09 23:27:45 +0200

    Created tests for Slider component

---
+++ src/__tests__/Slider.test.tsx
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react';
+import Slider from '../components/Slider';
+import slides from '../data/slides';
+
+describe('Slider component renders slider which shows different images', () => {
+    test('Main container - "div" with class "top-slider" was rendered correctly', () => {
+        const { container } = render(
+            <Slider
+                slides={slides}
+            />
+        );
+        const mainContainer = container.firstChild;
+        expect(mainContainer).toHaveClass('top-slider');
+    });
+
+    test('List of slides was rendered correctly', () => {
+        const { getByRole } = render(
+            <Slider
+                slides={slides}
+            />
+        );
+        const listOfSlides = getByRole('list');
+        expect(listOfSlides).toBeInTheDocument();
+    });
+});
